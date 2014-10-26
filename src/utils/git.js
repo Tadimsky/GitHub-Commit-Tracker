5ca32627620e87cac82a1cddc2186f9fa7833189
Jonno Schmidt <jonno@schmidtfam.us>
2014-10-26 17:16:52 -0400

    Fixed git util to support clone and update.

--- src/utils/git.js
+++ src/utils/git.js
@@ -1,6 +1,7 @@
 var nodegit = require("nodegit");
-var clone = require("nodegit").Repo.clone;
 var fs = require("fs");
+var rimraf = require("rimraf");
+
 //var clone = nodegit.Clone.clonerepo;
 
 /**
@@ -8,12 +9,16 @@ var fs = require("fs");
  * Takes in the repo url
  * */
 function repoInfo(repo) {
+    if (typeof repo != "string") {
+        return repo;
+    }
 //    repo = new String(repo);
     repo = "" + repo;
     var pattern = new RegExp(".*?(?:[a-z][a-z]+).*?(?:[a-z][a-z]+).*?(?:[a-z][a-z]+).*?((?:[a-z][a-z]+)).*?((?:[a-z][a-z]+))", ["i"]);
     var matches = pattern.exec(repo);
     var user = matches[1];
     var reponame = matches[2];
+
     var dirname = "/tmp/" + user + '/' + reponame;
 
     return {
@@ -24,22 +29,51 @@ function repoInfo(repo) {
     };
 }
 
-module.exports = {
-    cloneRepo : function(repo, callback) {
-        info = repoInfo(repo);
-        fs.exists(info.directory, function(exists) {
-            if (exists) {
-                return pullRepo(info, callback);
-            }
-            else {
-                //  clone repo
-                clone(info.url, info.directory, null).then(function(r) {
-                    console.log(r);
-                });
-            }
-        });
-    },
-    pullRepo : function(repo, callback) {
+function cloneRepository(repo, callback) {
+    info = repoInfo(repo);
+    fs.exists(info.directory, function(exists) {
+        if (exists) {
+            console.log("Repository exists, updating.");
+            return pullRepository(info, callback);
+        }
+        else {
+            console.log("Repository does not exist, cloning.");
+            nodegit.Clone.clone(info.url, info.directory, null).then(function(repo) {
+                console.log("\tRepository opened.");
+                console.log(r);
+                callback();
+            });
+        }
+    });
+}
 
-    }
+function pullRepository(rep, callback) {
+
+    rimraf(info.directory, function(err) {
+        if (err) throw err;
+        console.log("deleted " + info.directory);
+        cloneRepository(rep, callback);
+    });
+
+    // nodegit does not seem to support fetch yet...
+    /*
+    var repo = nodegit.Repository.open(rep.directory).then(function(re) {
+        console.log("\tRepository opened.");
+        nodegit.Remote.load(re, "origin").then(function (remote) {
+            console.log("\tRemote: origin loaded.");
+            console.log(remote);
+            remote.connect(0, function(err) {
+                console.log("Connected.");
+            });
+
+        }).catch(function(err) {
+            console.log(err);
+        });
+    });
+    */
+}
+
+module.exports = {
+    cloneRepo : cloneRepository,
+    pullRepo : pullRepository
 }
