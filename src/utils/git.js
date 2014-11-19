11bd4d2103f81182c42b50a3d04a6a77919282c4
Jonno Schmidt <jonno@schmidtfam.us>
2014-11-18 22:02:40 -0500

    Refactored git library

--- src/utils/git.js
+++ src/utils/git.js
@@ -8,6 +8,8 @@ var Promise = require("bluebird");
  * Returns the information required for a repo to be made.
  * Takes in the repo object from GitHub Event
  * */
+
+var defaultBranch = "gh-pages";
 function repoInfo(repo) {
     var dirname = "/tmp/" + repo.owner.name + '/' + repo.name;
 
@@ -20,146 +22,133 @@ function repoInfo(repo) {
     });
 }
 
-var defaultBranch = "gh-pages";
 
-function cloneRepository(rep) {
-    return new Promise(function(res, rej) {
-        fs.exists(rep.directory.repo, function(exists) {
-            if (exists) {
-                winston.info("Repository exists, updating.");
-                pullRepository(rep).then(function(repo) {
-                    return res(rep);
-                });
-            }
-            else {
-                winston.info("Repository does not exist, cloning.");
-                executeGit(rep.directory.repo, "clone " + rep.clone_url, true).then(function() {
-                    winston.info("\tRepository opened.");
-                    return res(rep);
-                }).catch(function(err) {
-                    throw err;
-                })
-            }
+/**
+ * Git Library
+ * @param repo: The repo information from GitHub events
+ * @constructor
+ */
+function Git(repo) {
+    var self = this;
+    var repository = repoInfo(repo);
+
+    var executeGit = function (command, lol) {
+        var dir = repository.directory.repo;
+        return new Promise(function (res, rej) {
+            fs.ensureDir(dir, function (err) {
+                var exec = require("child_process").exec;
+                var gitCommand = 'git';
+                gitCommand += lol ? ' ' : " --git-dir=" + dir + "/.git --work-tree=" + dir + " ";
+                command += lol ? ' ' + dir : '';
+                var git = exec(gitCommand + command,
+                    {
+                        cwd: dir,
+                        maxBuffer: 50 * 1024 * 1024
+                    },
+                    function (error, stdout, stderr) {
+                        if (error) rej(error);
+                        res(stdout);
+                    });
+            });
         });
-    });
-}
+    };
 
-function pullRepository(rep) {
-    return new Promise(function(res, rej) {
-        // nodegit does not seem to support fetch yet...
-        rimraf(rep.directory.repo, function(err) {
-            if (err) rej(err);
-            winston.info("deleted " + rep.directory.repo);
-            cloneRepository(rep).then(function(repo) {
-                res(repo);
-            })
-        });
-    });
-}
-
-function executeGit(dir, command, lol) {
-    return new Promise(function(res, rej) {
-        fs.ensureDir(dir, function(err) {
-            var exec = require("child_process").exec;
-            var gitCommand = 'git';
-            gitCommand += lol ? ' ' : " --git-dir=" + dir + "/.git --work-tree=" + dir + " ";
-            command += lol ? ' ' + dir : '';
-            var git = exec(gitCommand + command,
-                {
-                    cwd : dir,
-                    maxBuffer: 50 * 1024 * 1024
-                },
-                function(error, stdout, stderr) {
-                    if (error) rej(error);
-                    res(stdout);
-                });
-        });
-    });
-}
-
-function createEmptyBranch(dst, branch) {
-    return new Promise(function(res, rej) {
-        if (!branch) {
-            branch = defaultBranch;
-        }
-
-        nodegit.Repository.open(dst).then(function (rep) {
-            // no support for nodegit Branches yet.....
-
+    var checkoutOrphanBranch = function (branch) {
+        return new Promise(function (res, rej) {
             // checkout new orphan branch
             winston.info('Creating new ' + branch + ' branch');
-            executeGit(dst, "checkout --orphan " + branch).then(function () {
+            executeGit("checkout --orphan " + branch).then(function () {
                 // remove all files in current branch
                 winston.info('Created branch');
                 winston.info('Removing all files from branch');
-                executeGit(dst, "rm -f -r .").then(function () {
-                    winston.info('Cleaned folder ' + dst);
-                    // Create Readme file
+                executeGit("rm -f -r .").then(function () {
+                    winston.info('Cleaned folder.');
                     res();
                 });
             });
         });
-    });
-}
+    };
 
-function add(dst) {
-    return new Promise(function(resolve, reject) {
-        executeGit(dst, "add .").then(function() {
-            resolve();
-        }).catch(function(err) {
-            reject(err);
-        });
-    });
-}
+    return {
+        clone: function () {
+            return new Promise(function (res, rej) {
+                fs.exists(repository.directory.repo, function (exists) {
+                    if (exists) {
+                        winston.info("Repository exists, deleting and recloning.");
+                        rimraf(repository.directory.repo, function (err) {
+                            if (err) rej(err);
+                            self.clone().then(function () {
+                                res(repository);
+                            })
+                        });
+                    }
+                    else {
+                        winston.info("Repository does not exist, cloning.");
+                        executeGit("clone " + rep.clone_url, true).then(function () {
+                            winston.info("\tRepository cloned.");
+                            return res(rep);
+                        }).catch(function (err) {
+                            rej(err);
+                        })
+                    }
+                });
+            });
+        },
+        add: function () {
+            return new Promise(function (resolve, reject) {
+                executeGit("add .").then(function () {
+                    resolve();
+                }).catch(function (err) {
+                    reject(err);
+                });
+            });
+        },
+        commit: function (message) {
+            return new Promise(function (resolve, reject) {
+                executeGit("commit -m '" + message + "'").then(function () {
+                    resolve();
+                }).catch(function (err) {
+                    reject(err);
+                });
+            });
+        },
+        push: function (branch, force) {
+            var command = 'push -u ';
+            if (!branch) {
+                branch = 'master';
+            }
+            if (force) {
+                command += '-f ';
+            }
+            command += 'origin ' + branch;
 
-function commit(dst, message) {
-    return new Promise(function(resolve, reject) {
-        executeGit(dst, "commit -m '" + message + "'").then(function() {
-            resolve();
-        }).catch(function(err) {
-            reject(err);
-        });
-    });
-}
+            executeGit(command).then(function () {
+                resolve();
+            }).catch(function (err) {
+                reject(err);
+            });
+        },
+        pull: function (branch) {
 
-function push(dst, branch) {
-    return new Promise(function(resolve, reject) {
-        if (!branch) {
-            branch = defaultBranch;
-        }
-        executeGit(dst, "push -u -f origin " + branch).then(function() {
-            resolve();
-        }).catch(function(err) {
-            reject(err);
-        });
-    });
-}
-
-function createGitHubPages(repo) {
-    return new Promise(function(resolve, reject) {
-        var dst = repo.directory.repo;
-        var branch = "gh-pages";
-        createEmptyBranch(dst, branch).then(function() {
-            fs.writeFile(dst + "/Readme.md", "Documentation", function (err) {
-                if (err) throw err;
-                // add readme
-                add(dst).then(function () {
-                    commit(dst, "Added Readme").then(function () {
-                        winston.info('Committed Readme file.');
-                        resolve();
+        },
+        createGitHubPages: function() {
+            return new Promise(function (resolve, reject) {
+                var dst = repository.directory.repo;
+                var branch = "gh-pages";
+                checkoutOrphanBranch(branch).then(function () {
+                    fs.writeFile(dst + "/Readme.md", "Documentation", function (err) {
+                        if (err) reject(err);
+                        self.add().then(function () {
+                            self.commit("Added Readme").then(function () {
+                                winston.info('Committed Readme file.');
+                                resolve();
+                            });
+                        });
                     });
                 });
             });
-        });
-    });
+        }
+    }
 }
 
-module.exports = {
-    process : function(repo) {
-        return cloneRepository(repoInfo(repo));
-    },
-    setupGitHubPages: createGitHubPages,
-    add: add,
-    commit: commit,
-    push: push
-};
+module.exports = Git;
