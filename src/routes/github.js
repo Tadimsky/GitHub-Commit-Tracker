4bce6643fe9647062d032f0afb522642e565fa3d
Jonno Schmidt <jonno@schmidtfam.us>
2014-11-18 23:18:45 -0500

    revamped github route

--- src/routes/github.js
+++ src/routes/github.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var git = require('../utils/git.js');
+var Git = require('../utils/git.js');
 var doxygen = require('../utils/doxygen.js');
 var fs = require("fs-extra");
+var async = require('async');
 
 function handlePing(req) {
     winston.info('Ping!');
@@ -19,22 +20,25 @@ function handlePush(req) {
     winston.info("Head Commit: " + req.head_commit.id);
     winston.info("\tCommit Message: " + req.head_commit.message);
     winston.info("\tAuthor: " + req.head_commit.author.name + "(" + req.head_commit.author.username + ")");
-    git.process(req.repository).then(function(repo) {
-       doxygen.process(repo).then(function() {
-           git.setupGitHubPages(repo);
-           fs.copy(repo.directory.docs, repo.directory.repo, function(err){
-               if (err) return winston.error(err);
-               winston.info("Copied docs to repo.");
-               git.add(repo.directory.repo).then(function() {
-                   git.commit(repo.directory.repo, "Added Doxygen documentation").then(function() {
-                       git.push(repo.directory.repo, "gh-pages").then(function() {
-                          winston.info("Pushed Doxygen docs.");
-                           winston.profile('Git Push Event');
-                       });
-                   });
-               });
-           });
-       });
+
+    if (req.head_commit.author.username != 'Tadimsky') {
+        return;
+    }
+
+    var git = Git.Git(req.repository);
+
+    git.clone().then(function(repo) {
+        async.series([
+            function(callback) {
+                doxygen.process(git, repo).then(function() {
+                    callback(null, 'doxygen');
+                });
+            }
+        ], function(err, result) {
+            winston.info('Completed:')
+            winston.info('\t' + result);
+            winston.profile('Git Push Event');
+        });
     });
 }
 
